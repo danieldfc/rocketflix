7760c1600c6f687ec85190ea50ec3c3aa913b4df
bajinho <bajo@live.jp>
2020-04-09 16:24:24 -0300

    added class services/ErrorBoundary

---
+++ frontend/src/services/error.js
@@ -0,0 +1,24 @@
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Algo deu errado.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
