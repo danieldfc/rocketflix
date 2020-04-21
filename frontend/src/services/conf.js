5005663b756cb28e239fd79e46a4f2356673bc6f
bajinho <bajo@live.jp>
2020-04-21 17:47:04 -0300

    added const PATH axios

--- frontend/src/services/conf.js
+++ frontend/src/services/conf.js
@@ -1,3 +1,25 @@
-export const SERVER_URL_API = 'https://apiv2.rocketflix.com';
+export const SERVER_URL_API = "https://apiv2.rocketflix.com";
 
-export const TOKEN_KEY = '@Rocketflix-token';
+export const TOKEN_KEY = "@Rocketflix-token";
+
+export const PATH_USERS = "/users";
+
+export const PATH_SESSIONS = "/sessions";
+
+export const PATH_FILES = "/files";
+
+export const PATH_VIDEOS = "/videos";
+
+export const PATH_TAGS = "/tags";
+
+export const PATH_NOTIFICATIONS = "/notifications";
+
+export const PATH_REGISTER = "/register";
+
+export const PATH_DASHBOARD = "/dashboard";
+
+export const PATH_ACCOUNT = "/account";
+
+export const PATH_JOURNEY_VIDEOS = "/journey/videos";
+
+
