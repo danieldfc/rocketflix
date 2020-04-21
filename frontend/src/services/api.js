d52eacac84f7ce95af577774c9a2716d245bc75e
bajinho <bajo@live.jp>
2020-04-21 17:39:23 -0300

    added service apiGet apiPost apiDelete apiUpdate

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { getToken } from './auth';
-import { SERVER_URL_API } from './conf';
+import { getToken } from "./auth";
+import { SERVER_URL_API } from "./conf";
 
 const USER_TOKEN = getToken();
-const AuthStr = 'Bearer '.concat(USER_TOKEN);
+const AuthStr = "Bearer ".concat(USER_TOKEN);
 
 export const api = axios.create({
-  baseURL: SERVER_URL_API,
+  baseURL: SERVER_URL_API
 });
 
 api.interceptors.request.use(async config => {
@@ -16,3 +16,63 @@ api.interceptors.request.use(async config => {
   }
   return config;
 });
+
+export const apiGet = async path => {
+  try {
+    const res = await axios.get(`${SERVER_URL_API_V2}${path}`, {
+      headers: {
+        Authorization: AuthStr
+      }
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(`call for was ${SERVER_URL_API} cancelled: ${err}`);
+    throw err;
+  }
+};
+
+export const apiPost = async (path, param) => {
+  try {
+    const res = await axios.post(`${SERVER_URL_API_V2}${path}`, param, {
+      headers: {
+        Authorization: AuthStr
+      }
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(`call for was ${SERVER_URL_API} cancelled: ${err}`);
+    throw err;
+  }
+};
+
+export const apiDelete = async path => {
+  try {
+    const res = await axios.delete(`${SERVER_URL_API}${path}`, {
+      headers: {
+        Authorization: AuthStr
+      }
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(`call for was ${SERVER_URL_API} cancelled: ${err}`);
+    throw err;
+  }
+};
+
+export const apiUpdate = async (path, param) => {
+  try {
+    const res = await axios.put(`${SERVER_URL_API}${path}`, param, {
+      headers: {
+        Authorization: AuthStr
+      }
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(`call for was ${SERVER_URL_API} cancelled: ${err}`);
+    throw err;
+  }
+};
